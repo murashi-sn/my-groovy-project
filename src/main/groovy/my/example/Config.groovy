6454da2373a7854295e8d5d9525d2177338020a5
murashi-sn <nsa.toshi.shi@gmail.com>
2024-08-19 00:41:36 +0900

    wip

---
+++ src/main/groovy/my/example/Config.groovy
@@ -0,0 +1,5 @@
+package my.example
+
+user.name = 'fumokmm'
+user.kind = 'cat'
+user.is.programmer = true
--- src/main/groovy/my/example/GroovyMain.groovy
+++ src/main/groovy/my/example/GroovyMain.groovy
@@ -1,3 +1,22 @@
 package my.example
 
-println "Hello Groovy"
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+def config = new ConfigSlurper().parse(new File('Config.groovy').text)
+
+def client = new OkHttpClient()
+def request = new Request.Builder()
+        .url("https://example.com")
+        .build()
+
+def writer = new PrintWriter(new File("./output.txt"), "UTF-8")
+
+try (def response = client.newCall(request).execute()) {
+    println response.body().string()
+    println response.headers().forEach {
+        writer.println(it)
+    }
+}
+
+writer.close()
